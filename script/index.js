008822fe9d0dfbdf1586743a5fbe11324a37a811
zinniatasnim <zinnia660@gmail.com>
2025-08-29 12:00:05 +0600

    added call coin decrease function

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
-//heart-count
 let count= 0;
 function heart(){
     count++;
     document.getElementById("heart-count").innerText = count;
 }
 
-//call-alert
+let callButtonCount=0;
+let coin =100;
 document.getElementById("card-history").addEventListener("click", function(callButton){
     let btn = callButton.target;
     while(btn && !btn.classList.contains("call-btn")){
@@ -16,10 +16,25 @@ document.getElementById("card-history").addEventListener("click", function(callB
     while(card && ! card.classList.contains("card-1")){
         card= card.parentElement;
     }
+    if (coin < 20){
+        alert("❌ Insufficient Coin.Need 20 coins.");
+        return;
 
+    }
     const name = card.querySelector(".serviceName").innerText;
     const number = card.querySelector(".serviceNumber").innerText;
 
     alert("📞 Calling" +" "+ name +" "+number+" "+"...");
 
+    callButtonCount++;
+    coin = coin - 20;
+    document.getElementById("coin").innerText = coin; 
+    
+    
+
 })
+
+
+
+
+
