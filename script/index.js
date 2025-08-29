2619396d3b79278ef1820fc85c51fc61f043d973
zinniatasnim <zinnia660@gmail.com>
2025-08-29 11:36:49 +0600

    added call-alert

--- script/index.js
+++ script/index.js
@@ -1,5 +1,25 @@
+//heart-count
 let count= 0;
 function heart(){
     count++;
     document.getElementById("heart-count").innerText = count;
 }
+
+//call-alert
+document.getElementById("card-history").addEventListener("click", function(callButton){
+    let btn = callButton.target;
+    while(btn && !btn.classList.contains("call-btn")){
+        btn = btn.parentElement;
+    }
+    if (!btn) return;
+    let card = btn.parentElement;
+    while(card && ! card.classList.contains("card-1")){
+        card= card.parentElement;
+    }
+
+    const name = card.querySelector(".serviceName").innerText;
+    const number = card.querySelector(".serviceNumber").innerText;
+
+    alert("📞 Calling" +" "+ name +" "+number+" "+"...");
+
+})
